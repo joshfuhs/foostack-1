5c431acd33f2b89f79945a50a13f9219ba1db0af
Josh Fuhs <Joshua.Fuhs@pm.me>
2019-01-23 04:59:15 -0500

    Shut down all active instances on host shutdown

--- code/bin/store-run-state.sh
+++ code/bin/store-run-state.sh
@@ -13,3 +13,22 @@ DEVSTACK_HOME=/opt/stack/devstack
 
 source $DEVSTACK_HOME/openrc admin admin
 openstack server list --all-projects --host $(hostname) --status ACTIVE -c ID -f value > $RUNNING_INSTANCES_FILE
+cp $RUNNING_INSTANCES_FILE $RUNNING_INSTANCES_FILE.record
+openstack server list --all-projects --host $(hostname) -c ID -f value > $FOOSTACK_DATA/all_instances
+
+RUNNING_INSTANCES_TMPFILE=/tmp/foostack.store-$$-$RANDOM
+cp $RUNNING_INSTANCES_FILE $RUNNING_INSTANCES_TMPFILE
+
+LINE_COUNT=$(wc -l $RUNNING_INSTANCES_TMPFILE | awk '{ print $1 }')
+while [ $LINE_COUNT -gt 0 ]; do
+	for i in $(cat $RUNNING_INSTANCES_TMPFILE); do
+		# @todo: Will this work with a locked instance?
+		# This shouldn't trigger an error. We're just trying to make sure
+		# all servers get shut down.
+		openstack server stop $i || /bin/true
+	done
+	openstack server list --all-projects --host $(hostname) --status ACTIVE -c ID -f value > RUNNING_INSTANES_TMPFILE
+	LINE_COUNT=$(wc -l $RUNNING_INSTANCES_TMPFILE | awk '{ print $1 }')
+done
+
+rm $RUNNING_INSTANCES_TMPFILE
